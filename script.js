097edd1c69fccc37f2bab0aad1bcaf1b5f1e0d4d
chels <chelsleonhardt@gmail.com>
2023-12-28 16:27:14 -0500

    Recent work section created - used JS to highlight border on hover -added color variable for background on hover

--- script.js
+++ script.js
@@ -12,3 +12,41 @@ const closeMenu = document.getElementById('x-close');
 closeMenu.addEventListener('click', () => {
     mobileNavBar.style.display = 'none';
 });
+
+// Make border appear on image when hovering button
+
+//Hens and Honey
+const HensAndHoneyButton = document.getElementById('button-1');
+const HensAndHoneyImage = document.getElementById('image-1');
+
+HensAndHoneyButton.addEventListener('mouseover', () => {
+    HensAndHoneyImage.style.border = '3px solid var(--clr-clear-white)';
+});
+
+HensAndHoneyButton.addEventListener('mouseout', () => {
+    HensAndHoneyImage.style.border = 'none';
+});
+
+//Rosies
+const RosieButton = document.getElementById('button-2');
+const RosieImage = document.getElementById('image-2');
+
+RosieButton.addEventListener('mouseover', () => {
+    RosieImage.style.border = '3px solid var(--clr-clear-white)';
+});
+
+RosieButton.addEventListener('mouseout', () => {
+    RosieImage.style.border = 'none';
+});
+
+//SJ Rescue
+const SJRescueButton = document.getElementById('button-3');
+const SJRescueImage = document.getElementById('image-3');
+
+SJRescueButton.addEventListener('mouseover', () => {
+    SJRescueImage.style.border = '3px solid var(--clr-clear-white)';
+});
+
+SJRescueButton.addEventListener('mouseout', () => {
+    SJRescueImage.style.border = 'none';
+});
